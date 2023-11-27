7b81a5e3d71371895ee55d40626252a0baac10f0
isaacomololu <isaaccassij.omo@gmail.com>
2023-11-27 19:59:15 +0100

    task0 mod

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-var */
+ /* eslint-disable */
 export default function taskBlock(trueOrFalse) {
     var task = false;
     var task2 = true;
