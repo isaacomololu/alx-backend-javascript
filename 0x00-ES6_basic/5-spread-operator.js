4ebb0ab6a518839030383f69980bd10cd17b420f
isaacomololu <isaaccassij.omo@gmail.com>
2023-11-27 23:19:25 +0100

    task5 mod

--- 0x00-ES6_basic/5-spread-operator.js
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -1,4 +1,4 @@
 /* eslint-disable */
 export default function concatArrays(array1, array2, string) {
-    return spread = [...array1, ...array2, ...string]
+    return [...array1, ...array2, ...string]
 }
