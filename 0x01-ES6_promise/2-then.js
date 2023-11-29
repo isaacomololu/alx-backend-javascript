ffd0b32d8176e855bc7b66ccc659df9ae359e7cb
isaacomololu <isaaccassij.omo@gmail.com>
2023-11-29 01:00:04 +0100

    task2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function handleResponseFromAPI(promise) {
+    return promise
+        .then(() => ({
+            status: 200,
+            body: 'Success'
+        }))
+        .catch(() => new Error())
+        .finally(() => console.log('Got a response from the API'));
+}
