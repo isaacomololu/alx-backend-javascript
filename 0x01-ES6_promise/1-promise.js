fbbe9a13d8b05e446f82c0a9729a9b75e0b2c2c8
isaacomololu <isaaccassij.omo@gmail.com>
2023-11-29 02:10:22 +0100

    task1 okay

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,7 +2,7 @@
 export default function getFullResponseFromAPI(success) {
     const value = new Promise((resolve, reject) => {
         if (success) {resolve({ status: 200, body: 'Success' })} 
-        reject('Error: The fake API is not working currently');
+        reject(new Error('The fake API is not working currently'));
     });
     return value
 }
