18d18baa63945221c60c5973fcb6e4caf422644c
isaacomololu <isaaccassij.omo@gmail.com>
2023-11-29 02:01:47 +0100

    task3 moddd

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,12 @@
 /* eslint-disable */
-import { uploadPhoto , createUser } from 'utils.js';
-
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    const photo = uploadPhoto();
-    const user = createUser();
+  const photo = uploadPhoto();
+  const user = createUser();
 
-
-    Promise.all([photo, user])
-    .then((value) => {
-        console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
-    })
-    .catch(() => {console.log('Signup system offline')});
+  return Promise.all([photo, user]).then((value) => {
+    console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
