4abced7d3a527039e9624c6babd7c4f49d4dc549
isaacomololu <isaaccassij.omo@gmail.com>
2023-11-30 16:13:49 +0100

    task6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import { signUpUser } from 4-user-promise.js;
+import { uploadPhoto } from 5-photo-reject.js;
+
+export default function handleProfileSignup(firstName, lastName, fileName ) {
+    const signUpUserPromise = signUpUser(firstName, lastName);
+    const uploadPhotoPromise = uploadPhoto(fileName);
+
+    return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
+        .then((values) => {
+            values.forEach((element) => {
+                if (element.status === 'fulfilled') {
+                  result.push({ status: element.status, value: element.value });
+                } else {
+                  result.push({ status: element.status, value: `${element.reason}` });
+                }
+              });
+              return result;
+            });
+       
+}
