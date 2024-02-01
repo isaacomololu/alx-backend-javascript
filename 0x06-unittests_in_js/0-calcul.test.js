a1d273869e2a0aff8bd21f8597a0c9f1bc155e3e
isaacomololu <isaaccassij.omo@gmail.com>
2024-02-01 13:06:01 +0100

    expresssssss

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,22 +1,19 @@
-const calculateNumber = require("./0-calcul.js");
+const calculateNumber = require('./0-calcul.js');
 const assert = require('assert');
 
-describe('calculateNumber', () => {
-    it('rounding of a', () => {
-        assert.equal(calculateNumber(15.78, 2), 18);
-        assert.equal(calculateNumber(1.2, 0), 1);
-        assert.equal(calculateNumber(3.5, 2), 6);
-    });
+describe('Test Suite', function () {
+  it('Test first argument float', function() {
+    assert.strictEqual(calculateNumber(1.1, 2), 3);
+    assert.equal(calculateNumber(1.2, 2), 3);
+  });
 
-    it('rounding of b', () => {
-        assert.equal(calculateNumber(2, 15.78), 18);
-        assert.equal(calculateNumber(0, 1.2), 1);
-        assert.equal(calculateNumber(2, 3.5), 6);
-    });
+  it('Test second argument float', function() {
+    assert.equal(calculateNumber(1, 2.1), 3);
+    assert.equal(calculateNumber(1, 2.3), 3);
+  });
 
-    it('suming of a and b', () => {
-        assert.equal(calculateNumber(15.78, 2.5), 19);
-        assert.equal(calculateNumber(1.2, 0.2), 1);
-        assert.equal(calculateNumber(3.5, 2.7), 7);
-    });
-})
+  it('Test both argument float', function() {
+    assert.equal(calculateNumber(1.0, 2.0), 3);
+    assert.equal(calculateNumber(1.2, 2.4), 3);
+  });
+});
--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,19 +1,19 @@
-function calculateNumber(type, a, b) {
-    const rounded_a = Math.round(a);
-    const rounded_b = Math.round(b);
-    
-    if (type === 'SUM') {
-      return rounded_a + rounded_b;
-    }
-    if (type === 'SUBTRACT') {
-      return rounded_a - rounded_b;
-    }
-    if (type === 'DIVIDE') {
-      if (rounded_b === 0) {
-        return 'Error';
-      }
-      return rounded_a / rounded_b;
-    }
+function calculateNumber (type, a, b) {
+  a = Math.round(a);
+  b = Math.round(b);
+
+  if (type === 'SUM') {
+    return a + b;
   }
-  
-  module.exports = calculateNumber;
+  if (type === 'SUBTRACT') {
+    return a - b;
+  }
+  if (type === 'DIVIDE') {
+    if (b === 0) {
+      return 'Error';
+    }
+    return a / b;
+  }
+}
+
+module.exports = calculateNumber;
